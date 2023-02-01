de9ca99e8d6d738f491574dac807051dd9bd9553
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2023-01-31 19:26:17 -0600

    commit

--- movie-rater/src/components/movie-details.js
+++ movie-rater/src/components/movie-details.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
-
-
-
-
 function MovieDetails(props){
 
+    const [ highlighted, setHighlighted ] = useState(3);
+
+    const mov = props.movie;
+
+
     return (
-        <div>
-            { props.movie ? (
+        <React.Fragment>
+            { mov ? (
                 <div>
-                    <h1>{props.movie.title}</h1>
-                    <p>{props.movie.description}</p>
-                    <FontAwesomeIcon icon={faStar} />
+                    <h1>{mov.title}</h1>
+                    <p>{mov.description}</p>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 0 ? 'orange':''}/>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 1 ? 'orange':''}/>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 2 ? 'orange':''}/>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 3 ? 'orange':''}/>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 4 ? 'orange':''}/>
+                    ({mov.no_of_ratings})
+                    <div className="rate-container">
+                        <h2>Rate it</h2>
+                        { [...Array(5)].map( (e, i) => {
+                            return <FontAwesomeIcon icon={faStar} className={highlighted > i -1 ? 'purple':''}/>
+                        })}
+                    </div>
                 </div>
             ) : null }
-        </div>
+        </React.Fragment>
     )
 }
 
