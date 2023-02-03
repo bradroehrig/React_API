d839fd8398cc32332e9e86e807b3611363e76fa0
bradroehrig <63978952+bradroehrig@users.noreply.github.com>
2023-02-03 13:39:34 -0600

    commit

--- movie-rater/src/components/movie-list.js
+++ movie-rater/src/components/movie-list.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 function MovieList(props){
 
@@ -6,12 +9,19 @@ function MovieList(props){
         props.movieClicked(movie)
     }
 
+    const editClicked = movie => {
+        props.editClicked(movie);
+    }
+
+
     return (
         <div>
             { props.movies && props.movies.map( movie => {
                 return (
                 <div key={movie.id}>
                     <h2 onClick={movieClicked(movie)}>{ movie.title }</h2>
+                    <FontAwesomeIcon icon={faEdit} onClick={() => editClicked(movie)}/>
+                    <FontAwesomeIcon icon={faTrash}/>
                 </div>
                 )
             })}
