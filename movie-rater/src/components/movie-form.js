67b6fb0b28d325f3dd8e74b538498c310712e5b1
bradroehrig <63978952+bradroehrig@users.noreply.github.com>
2023-02-03 14:41:19 -0600

    commit

--- movie-rater/src/components/movie-form.js
+++ movie-rater/src/components/movie-form.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function MovieForm(props) {
+
+    const [ title, setTitle] = useState(props.movie.title);
+    const [ description, setDescription] = useState(props.movie.description);
+
+    const updateClicked = () => 
+        console.log('update here')
+
+
+
+
     return (
-            <React.Fragment>
+        <React.Fragment>
             { props.movie ? (
-                <h1>{props.movie.title} edit</h1>
+                <div>
+                    <label for="title">Title</label><br/>
+                    <input id="title" type="text" placeholder="title" value={title}
+                        onChange={ evt => setTitle(evt.target.value)}
+                    /><br/>
+                    <label for="description">Description</label><br/>
+                    <textarea id="description" type="text" placeholder="description" value={description}
+                        onChange={ evt => setDescription(evt.target.value)}
+                    ></textarea><br/>
+                    <button onClick={updateClicked} >Update</button>
+                </div>
             ) : null}
-            </React.Fragment>
+        </React.Fragment>
     )
 }
 
