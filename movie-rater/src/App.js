2126518fb62ef4b8919aee751e9b64bcaa66c693
bradroehrig <63978952+bradroehrig@users.noreply.github.com>
2023-02-03 13:59:14 -0600

    commit

--- movie-rater/src/App.js
+++ movie-rater/src/App.js
@@ -23,19 +23,15 @@ function App() {
     .catch( error => console.log(error))
   }, [])
 
-  const movieClicked = movie => {
-    setSelectedMovie(movie);
-  }
-
   const loadMovie = movie => {
     setSelectedMovie(movie);
+    setEditedMovie(null);
   }
 
   const editClicked = movie => {
     setEditedMovie(movie);
+    setSelectedMovie(null);
   }
-
-
 
   return (
     
@@ -44,7 +40,7 @@ function App() {
         <h1>Movie Rater</h1>
       </header>
       <div className="layout">
-        <MovieList movies={movies} movieClicked={movieClicked} editClicked={editClicked}/>
+        <MovieList movies={movies} movieClicked={loadMovie} editClicked={editClicked}/>
         <MovieDetails movie={selectedMovie} updateMovie={loadMovie}/>
         <MovieForm movie={editedMovie}/>
       </div>
