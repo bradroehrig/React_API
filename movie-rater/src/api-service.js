1e9e3076a281a99c31e970f93d1126d7aa1c3156
bradroehrig <63978952+bradroehrig@users.noreply.github.com>
2023-02-06 12:54:12 -0600

    commit

--- movie-rater/src/api-service.js
+++ movie-rater/src/api-service.js
@@ -1,6 +1,20 @@
 const TOKEN = "0b5b3e40e093f6746767bed29354889f58bdaf0e";
 
 export class API {
+    static loginUser(body) {
+        return fetch(`http://127.0.0.1:8000/auth/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify( body )
+        }).then( resp => resp.json())
+    }
+
+
+
+
+
     static updateMovie(mov_id, body) {
         return fetch(`http://127.0.0.1:8000/api/movies/${mov_id}/`, {
             method: 'PUT',
--- movie-rater/src/index.js
+++ movie-rater/src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Auth from './components/auth';
 import reportWebVitals from './reportWebVitals';
+import {Route, Routes, BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+export const TokenContext = createContext(null);
+
+function Router(){
+
+  const [token, setToken] = useState('');
+
+
+  return (
+
+    <React.StrictMode>
+      <TokenContext.Provider value={{token, setToken}}>      
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" exact element={ <Auth/> } />
+            <Route path="/movies" exact element={ <App/> } />
+          </Routes>
+        </BrowserRouter>
+      </TokenContext.Provider>
+    </React.StrictMode>
+  )
+}
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root'),
+//   );
+// root.render();
+
+ReactDOM.createRoot(
+  document.getElementById("root"),
+)
+.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router />
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
